a01ad5efd7326b9eb2729bd6ce3be1f4f2cb3c43
Maddie Ritter <m1ritter@UCSD.EDU>
2023-02-02 08:17:57 -0800

    added a comment

--- Server.java
+++ Server.java
@@ -1,7 +1,7 @@
 // A simple web server using Java's built-in HttpServer
 
 // Examples from https://dzone.com/articles/simple-http-server-in-java were useful references
-
+//
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
